7325e2ccdb9a7d8b61f7e7546e4b59adf286825c
ebA <121199967+spac3-dust@users.noreply.github.com>
2026-08-12 16:34:05 -0400

    Use favicon as header logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import { FaLayerGroup } from 'react-icons/fa';
 import './Header.css';
 
 const Header = () => {
@@ -11,9 +10,14 @@ const Header = () => {
         <Navbar className="site-header">
             <Container className="site-header__inner">
                 <Navbar.Brand className="site-brand" href={homeUrl}>
-                    <span className="site-brand__mark" aria-hidden="true">
-                        <FaLayerGroup />
-                    </span>
+                    <img
+                        className="site-brand__mark"
+                        src="/favicon-eduthemes-192.png"
+                        alt=""
+                        width="36"
+                        height="36"
+                        aria-hidden="true"
+                    />
                     <span className="site-brand__name">EduThemes</span>
                 </Navbar.Brand>
             </Container>
